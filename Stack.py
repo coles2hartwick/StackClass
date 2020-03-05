e244530d87bc913f0737efe7ee297184679bc51f
scole <coles2@hartwick.edu>
2020-03-04 20:56:02 -0500

    did it

--- Stack.py
+++ Stack.py
@@ -1,2 +1,15 @@
 # Sam Cole
 # This is the stack class
+
+from LinkedList import LinkedList
+
+
+class Stack:
+    def __init__(self):
+        self.myList = LinkedList()
+
+    def push(self, data):
+        self.myList.add_to_head(data)
+
+    def pop(self):
+        self.myList.remove_end()
